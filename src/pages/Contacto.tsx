fd283b26afb30ebe7b4ea2d5ff7bc714fffa740e
LUCIezq <lucieze02@icloud.com>
2025-07-18 13:26:42 -0300

    UPDATE: Vista contacto

--- src/pages/Contacto.tsx
+++ src/pages/Contacto.tsx
@@ -7,7 +7,7 @@ export default function Contacto() {
         <Main>
             <Title text="Contacto" />
             <div className="flex flex-col items-center justify-center gap-10 p-10 ">
-                <div className="text-white bg-black  w-full max-w-[1200px] flex gap-40 p-8 justify-center rounded-3xl shadow-2xl shadow-[#ffffff23]">
+                <div className="text-white bg-black  w-full max-w-[1200px] flex gap-40 p-8 justify-center rounded-3xl shadow-2xl shadow-[#ffffff12]">
                     <div className="flex flex-col justify-center items-start gap-4">
                         <span className="border-1 border-[#ffffff63] p-2 rounded-md text-[12px]">
                             ¿Como podes contactarnos?
